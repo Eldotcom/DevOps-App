8ec95b1af74c8accb346b83a2b2fbeeddcc784d3
Eldotcom <elivni76@gmail.com>
2025-04-07 22:21:02 +0300

    Fix: expose app to public in Render

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask
 from routes.dashboard import dashboard_bp
 from routes.auth import auth_bp
 from routes.monitoring import monitoring_bp
+import webbrowser
+import threading
 
 app = Flask(__name__)
 app.secret_key = "dev_secret"
@@ -11,5 +13,12 @@ app.register_blueprint(dashboard_bp)
 app.register_blueprint(monitoring_bp)
 app.register_blueprint(auth_bp)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # פותח את הדפדפן אחרי הפעלה
+    def open_browser():
+        webbrowser.open_new("http://127.0.0.1:5000/login")
+
+    # מחכה שניה ופותח דפדפן
+    threading.Timer(1, open_browser).start()
+
     app.run(debug=True)
